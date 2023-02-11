5ff0db8a57de3505101f5e09758586ada1799139
ThanOs <lads42279@gmail.com>
2023-02-12 00:10:13 +0530

    String done

---
+++ String.js
@@ -0,0 +1,9 @@
+var name ='shubham';
+var nickname='ThanOs';
+var profection ='Fronted Developer(Beginner)';
+
+var temp =`${name} have nickname ${nickname} and he is a ${profection}`;
+console.log(temp)
+
+var len=name.length;
+console.log(len)

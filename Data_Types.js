2b2a217659ea437ee414a44097d3bc2bd0925dbb
ThanOs <lads42279@gmail.com>
2023-02-12 11:00:29 +0530

    Data Types done

---
+++ Data_Types.js
@@ -0,0 +1,17 @@
+
+
+let number =5;                  //number
+console.log(typeof(number))
+
+let username = "ImThanOs36";   //string
+console.log(typeof(username))
+
+let null_val= null;             // NULL or Object
+console.log(typeof(null_val))
+console.log(null_val)
+
+let undefined_val= undefined;   //undefined
+console.log(typeof(undefined_val))
+
+let boolean_val= true;          //Boolean
+console.log(typeof(boolean_val))

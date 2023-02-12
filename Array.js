15fbffbad29ed4da593b2f7b135fdf770b56e0fd
ThanOs <lads42279@gmail.com>
2023-02-12 13:50:47 +0530

    Array done

---
+++ Array.js
@@ -0,0 +1,12 @@
+let numbers=[1,2,3,4,5,6,7,8,9,10];
+
+let names=['ThanOs','Shinchan','Vision','Venom'];
+
+let array=['ThanOs',3,true,null,undefined];
+
+console.log(numbers)
+console.log(names)
+console.log(array)
+
+let num=(3);
+console.log(num[0]);

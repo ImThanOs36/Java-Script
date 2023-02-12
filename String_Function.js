30b43c35d3c04e6488494240aa265407a9a52a30
ThanOs <lads42279@gmail.com>
2023-02-12 10:07:48 +0530

    String Functions done !

---
+++ String_Function.js
@@ -0,0 +1,47 @@
+var string='this is a string'
+console.log(string)
+
+
+var position = string.indexOf('is');
+console.log(position)
+
+
+var position = string.lastIndexOf('is');
+console.log(position)
+
+var string_slice = string.slice(1, 6)
+console.log(string_slice)
+
+var substr = string.substr(0,6);
+console.log(substr)
+
+var replace = string.replace('string', 'some texts');
+console.log(replace);
+
+var uppercase= string.toUpperCase();
+console.log(uppercase);
+
+
+var lowercase= string.toLowerCase();
+console.log(lowercase);
+
+
+var concat = string.concat(' Im new');
+console.log(concat);
+
+var string_2 = "   This is string     with    spaces on start and end           "
+var trim = string.trim();
+console.log(string_2);
+console.log(trim);
+
+
+var char_code = string.charCodeAt(2);
+console.log(char_code)
+
+var charAt = string.charAt(2);
+console.log(charAt);
+
+var code_of_char = string.charCodeAt(2);
+console.log(code_of_char)
+
+

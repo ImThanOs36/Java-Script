3537f45be84567a0c393c81eccc041088ac15259
ThanOs <lads42279@gmail.com>
2023-02-12 10:23:00 +0530

    Else_If done

---
+++ Else_if.js
@@ -0,0 +1,14 @@
+
+let age = 26;
+
+if(age>18){
+
+    console.log("You can drive cars and ride bikes");
+}
+
+else if(age==17){
+    console.log("After 1 year you will drive or ride");
+}
+else{
+    console.log("You cant drive or ride")
+}

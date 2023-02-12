943e2591f7d527e532fea2047e5bb2bf45c7c2ed
ThanOs <lads42279@gmail.com>
2023-02-12 16:57:43 +0530

    Greet Function with Returning a value

---
+++ Greet_Function_returning_Value.js
@@ -0,0 +1,18 @@
+function greet(name){
+
+    let greet = "Hello, "+name ;
+    return greet;
+    
+    }
+    
+    
+    let name_1="ThanOs";
+    let name_2="Vision";
+    let name_3="ShinChan";
+    let name_4="Venom";
+    console.log(greet(name_1));
+    console.log(greet(name_2));
+    console.log(greet(name_3));
+    console.log(greet(name_4));
+    
+    

dd3a6e59b532ee31a43c553196e320d44379589e
ThanOs <lads42279@gmail.com>
2023-02-12 10:26:00 +0530

    Switch_case Done

---
+++ Switch_Case.js
@@ -0,0 +1,11 @@
+let cake = 1;
+
+switch (cake) {
+    case 1:
+        console.log("Value of cake is 1");
+        break;
+   case 2:
+    console.log("Value of cake is 2")
+    default:
+        break;
+}

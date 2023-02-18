8953a69e67a838ed61ebc52b9f76c611adedba88
ThanOs <lads42279@gmail.com>
2023-02-18 14:52:10 +0530

    Arrow function half part done

---
+++ ArrowFunction.js
@@ -0,0 +1,21 @@
+
+
+let greet= ()=> console.log("Hello");
+greet();
+
+let name="hey"
+let greeting=(name)=> console.log(name+" ,hey");
+greeting(name);
+
+
+let object={
+
+    name:["Hello","thanos"],
+    speek(){
+
+        this.name.forEach(function (student){
+            console.log()
+        });
+
+    }
+}

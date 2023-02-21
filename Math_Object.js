4653a8e18003c60213817654f092f8cdc360c4f8
ThanOs <lads42279@gmail.com>
2023-02-21 23:06:14 +0530

    Math_Object Started

---
+++ Math_Object.js
@@ -0,0 +1,9 @@
+let m = Math;
+    console.log(m)
+
+    console.log("The value of Math.E is ", Math.E)
+    console.log("The value of Math.PI is ", Math.PI)
+    console.log("The value of Math.LN2 is ", Math.LN2)
+    console.log("The value of Math.SQRT1_2 is ", Math.SQRT1_2)
+    console.log("The value of Math.LOG2E is ", Math.LOG2E)
+    console.log("Maximum value of 4, 5, 6 is ", Math.max(4,5, 6));

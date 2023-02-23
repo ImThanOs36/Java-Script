2aa541950e3dd01d623788ad5e72fcedd9f17978
ThanOs <lads42279@gmail.com>
2023-02-23 18:32:48 +0530

    Tryed Some Math_Methods

---
+++ Math-Method_1.js
@@ -0,0 +1,25 @@
+
+// Math.round(x)     = Returns x rounded to its nearet Integer
+console.log("Math.round(x)     = Returns x rounded to its nearet Integer");
+console.log(Math.round(4.1));
+console.log(Math.round(4.5));
+console.log(Math.round(4.6));
+
+// Math.ceil(x)      = Returns x rounded up to its nearest integer
+console.log(" Math.ceil(x)      = Returns x rounded up to its nearest integer")
+console.log(Math.ceil(1.1));
+console.log(Math.ceil(1.5));
+console.log(Math.ceil(1.8));
+
+//Math.floor(x)      = Returns x rounded down to its nerest integer
+console.log("Math.floor(x)      = Returns x rounded down to its nerest integer")
+console.log(Math.floor(1.1));
+console.log(Math.floor(1.5));
+console.log(Math.floor(1.8));
+
+//Math.trunc(x)      = Returns The Integer part of x (New in ES6)
+console.log("Math.trunc(x)      = Returns The Integer part of x (New in ES6)")
+console.log(Math.trunc(3.1));
+console.log(Math.trunc(3.5));
+console.log(Math.trunc(3.8));
+

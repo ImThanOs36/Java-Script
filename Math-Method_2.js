3a32fb16b06769686b22c1cda7d7841b10e0bacb
ThanOs <lads42279@gmail.com>
2023-02-23 18:45:46 +0530

    Explored other Math_Methods

---
+++ Math-Method_2.js
@@ -0,0 +1,26 @@
+//Math.sign(x)          = Returns if x is negative , Null or Positive
+
+console.log("Math.sign(x)          = Returns if x is negative , Null or Positive");
+console.log(Math.sign(0));
+console.log(Math.sign(5));
+console.log(Math.sign(-89));
+console.log(Math.sign(null));
+
+//Math.pow(x)           = Returns the value of X to The power of Y
+console.log("Math.pow(x)           = Returns the value of X to The power of Y");
+console.log(Math.pow(8, 1));
+console.log(Math.pow(4, 3));
+console.log(Math.pow(12, 0));
+
+//Math.sqrt(x)          =Returns the Square root of x.
+console.log("Math.sqrt(x)          =Returns the Square root of x.");
+console.log(Math.sqrt(0));
+console.log(Math.sqrt(13));
+console.log(Math.sqrt(36));
+console.log(Math.sqrt(64));
+
+//Math.abs(x)           = Returns the absolute (Postive) Value of X
+console.log("Math.abs(x)           = Returns the absolute (Postive) Value of X");
+console.log(Math.abs(-4));
+console.log(Math.abs(-9.78));
+console.log(Math.abs(9));
